23bceaf3cb1e0ba21220f07134b09ea53929c7b1
Shahhum1 <48683577+Shahhum1@users.noreply.github.com>
2023-08-23 19:29:13 +0500

    Save changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/fc2.png";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "our about backgroung";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a web developer student studying at SMIU University. I am doing freelancing for last 3 years, I love developing dynamic custom websites";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
+  "Web Development",
+  "UI/UX",
+  "Figma design",
+  "Ecommerce Development",
   "Mobile user interfaces",
-  "Graphic design",
+  "Search Engine Optimization",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "This is my portfolio website, I am selling custom website. I am runnig an online software house named Dot 1 innovations. I am here to provide best digital services all over the world.";
 
 const About = () => {
   return (
